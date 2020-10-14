8b7bab8174472c330f1c43aa44cb73a1e44541af
fter321 <hc8hc9hc8u@mail.ru>
2020-10-14 16:21:23 +0900

    -

--- main.py
+++ main.py
@@ -8,25 +8,35 @@ W = 480
 H = 360
 SILVER = (192, 192, 192)
 BLACK = (0, 0, 0)
+numetal = ''
+move = 1
+block = 0
+start = 1
+OUTSIDE_BG =(0, -100)
 
 pygame.init()
 pygame.display.set_caption('Угадай число')
 screen = pygame.display.set_mode((W, H))
 pygame.mouse.set_visible(False)
 
+font = pygame.font.SysFont('Arial', 28, True, False)
+font2 = pygame.font.SysFont('Arial', 14, False, True)
+font_box = pygame.Surface((W - 20, font.get_height()))
+font_rect = font_box.get_rect(center=(W // 2, H - font.get_height()))
+
+bg = pygame.image.load('Image/room.png') 
+bg_rect = bg.get_rect(topleft=(0, 0))
+
 cat = pygame.image.load('Image/cat.png')
-cat_rect = cat.get_rect(center=(70,220))
+cat_rect = cat.get_rect(center=(70, 220))
 
 dog = pygame.image.load('Image/dog.png')
-dog_rect = dog.get_rect(center=(410,220))
+dog_rect = dog.get_rect(center=(410, 220))
 
-owl = pygame.image.load('Image/room.png')
-owl_rect = owl.get_rect(center=(210,120))
+owl = pygame.image.load('Image/owl.png')
+owl_rect = owl.get_rect(center=(210, 120))
 
-bg = pygame.image.load('Image/room.png')
-bg_rect = bg.get_rect(topleft=(0,0))
-
-dialog = pygame.image.load('Image/room.png')
+dialog = pygame.image.load('Image/dialog.png')
 dialog_rect = dialog.get_rect()
 dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
 dialog_dog_pos = (dog_rect.x, dog_rect.w // 2, dog_rect.y - dialog_rect.h)
@@ -38,10 +48,9 @@ while run:
             run = False
 
     screen.blit(bg, bg_rect)
-    pygame.display.update()
-
-    screen.blit(bg, gb_rect)
     screen.blit(cat, cat_rect)
     screen.blit(dog, dog_rect)
-    screen.blit(owl, wol_rect)
-    
+    screen.blit(owl, owl_rect)
+    screen.blit(font_box, font_rect)
+    font_box.fill(SILVER)
+    pygame.display.update()
