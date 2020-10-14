6305e7f3e8e11c4c350cd7c722edb843744e6420
fter321 <hc8hc9hc8u@mail.ru>
2020-10-14 15:13:15 +0900

    -

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import random
+
+num = random.randint(0, 100)
+print(num)
+
+W = 480
+H = 360
+SILVER = (192, 192, 192)
+BLACK = (0, 0, 0)
+
+pygame.init()
+pygame.display.set_caption('Угадай число')
+screen = pygame.display.set_mode((W, H))
+pygame.mouse.set_visible(False)
+
+run = True
+while run:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            run = False
